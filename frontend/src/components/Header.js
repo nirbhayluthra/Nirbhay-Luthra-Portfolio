70cff3dc43595366522d1da90f9b81d163ef1796
nirbhayluthra <47568308+nirbhayluthra@users.noreply.github.com>
2021-05-23 23:46:31 +0530

    sec comit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,11 +4,7 @@ const Header = () => {
   return (
     <nav className='space-x-10'>
       <ul className='flex flex-row space-x-4 md:space-x-10 md:text-xl justify-center md:justify-end font-thin'>
-           <li>
-          <a href='#abtme' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
-            About Me
-          </a>
-        </li>
+
         <li>
           <a href='#projects' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
             Projects
